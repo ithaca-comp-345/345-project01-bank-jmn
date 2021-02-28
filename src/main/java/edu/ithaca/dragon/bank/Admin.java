30009346fee33f28d40a16a207ff2300051fa814
NusiratO <nolumegbon@ithaca.edu>
2021-02-28 17:24:24 -0500

    Added the first test in Admin & a couple of lines in Admin

--- src/main/java/edu/ithaca/dragon/bank/Admin.java
+++ src/main/java/edu/ithaca/dragon/bank/Admin.java
@@ -1,5 +1,30 @@
 package edu.ithaca.dragon.bank;
 
 public class Admin {
+
+    private CentralBank bank;
+    private boolean freezeAccount;
+    private UserAccount accountNum;
+
+	public Admin(int num) {
+        freezeAccount = false;
+        accountNum = bank.getUserAccount(num);
+	}
+
+
+	public boolean freezeOrUnFreezeAccount(boolean freezeOrUnFreeze){
+        return true;
+	}
+
+    public int getAccountNumber() {
+        return accountNum.getAccountNumber();
+    }
+
+    public boolean getFreezeAccount(){
+        return getFreezeAccount();
+    }
+    
+    
+
     
 }
--- src/test/java/edu/ithaca/dragon/bank/AdminTest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminTest.java
@@ -1,5 +1,24 @@
 package edu.ithaca.dragon.bank;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+
 public class AdminTest {
     
+    @Test
+    void freezeUnFreezeAccountTest(){
+        //Just freezes the account Number
+        CentralBank bank = new CentralBank();
+        BankTeller teller = new BankTeller(bank);
+        teller.addUserAccount("2222", 100);
+        Admin newAdmin = new Admin(bank.getUserAccount(100).getAccountNumber());
+        assertEquals(false, newAdmin.getFreezeAccount());
+        newAdmin.freezeOrUnFreezeAccount(true);
+        assertEquals(true, newAdmin.getFreezeAccount());
+    }
+
+
 }
